edbd2c16065a573bf77209beb6c7ca268e024e05
felipesandrade <felipes.andrade@gmail.com>
2022-09-27 17:48:41 -0300

    add like comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,18 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 
 import styles from './Comment.module.css';
 
 export function Comment({content, onDeleteComment}) {
+    const [likeCount, setLikeCount] = useState(0);
+
     function handleDeleteComment() {
         onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1);
     }
 
     return(
@@ -34,9 +41,9 @@ export function Comment({content, onDeleteComment}) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Apludir <span>20</span>
+                        Apludir <span>{likeCount}</span>
                     </button>
                 </footer>
                 
